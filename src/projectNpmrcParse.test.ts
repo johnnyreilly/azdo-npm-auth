b6e9cca29ba27f0ea48a93c347456e9ffcdc4603
John Reilly <johnny_reilly@hotmail.com>
2024-12-10 12:24:58 +0000

    feat: support org scoped npmrc

--- src/projectNpmrcParse.test.ts
+++ src/projectNpmrcParse.test.ts
@@ -27,6 +27,22 @@ always-auth=true`);
 		});
 	});
 
+	it("outputs the expected structure on successful parse with org scope", async () => {
+		mockReadFile.mockResolvedValue(`@myorg:registry=https://pkgs.dev.azure.com/johnnyreilly/_packaging/npmrc-script-organization/npm/registry/ 
+                        
+always-auth=true`);
+		const result = await projectNpmrcParse({
+			npmrcPath: "/home/john/code/github/azdo-npm-auth/.npmrc",
+		});
+		expect(result).toEqual({
+			organization: "johnnyreilly",
+			urlWithoutRegistryAtEnd:
+				"//pkgs.dev.azure.com/johnnyreilly/_packaging/npmrc-script-organization/npm/",
+			urlWithoutRegistryAtStart:
+				"//pkgs.dev.azure.com/johnnyreilly/_packaging/npmrc-script-organization/npm/registry/",
+		});
+	});
+
 	// I haven't worked out whether I want to support this yet
 	it.skip("outputs the expected structure when expected last `/` is not there", async () => {
 		mockReadFile.mockResolvedValue(`registry=https://pkgs.dev.azure.com/johnnyreilly/_packaging/npmrc-script-organization/npm/registry 
